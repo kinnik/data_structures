ad7e639e9371fd608aa2bf8e157e638349d2f328
kinnik <northislandking@gmail.com>
2014-11-11 20:27:09 +0000

    White space and indentation

--- arrays/SpecialResizingArray.java
+++ arrays/SpecialResizingArray.java
@@ -1,25 +1,35 @@
-/* A special resizing array that supports the below operations in amortised O(1) time:
-   void insert(Item item);
-   void remove(Item item);
-   boolean contains(Item item);
-   Item random();
+/* A special resizing array that supports the below operations in O(1) or 
+   amortised O(1) time:
+       void insert(Item item);
+       void remove(Item item);
+       boolean contains(Item item);
+       Item random();
 
    Implementation
    ==============
    The resizing array supports insert and random in O(1) time.
-   In order to support remove and contains, we use hash tables.
+   In order to support remove and contains in O(1), we use a hash table.
 
-   insert – Everytime an item is inserted, we record that item's position in the hash table. The hash table thus stores the key value pair of (item, position).
+   insert – Everytime an item is inserted, we record that item's position in the
+   hash table. The hash table thus stores the key value pair of (item, position).
 
-   remove – To remove the given item in O(1) time, we need to know its position. This position is already recorded in the hash table.
-   (i)  We therefore retrieve the position first from the hash table in O(1) amortised time.
-   (ii) Removing the item from its current position means setting it to null. This will however create a gap in the array. To prevent this, we fill the gap with the last item in the array and take care of the last-index accordingly.
+   remove – To remove the given item in O(1) time, we need to know its position.
+   This position is already recorded in the hash table.
+   (i)  We therefore retrieve the position first from the hash table in O(1)
+        amortised time.
+   (ii) Removing the item from its current position means setting it to null.
+        This will however create a gap in the array. To prevent this, we fill
+        the gap with the last item in the array and take care of the last-index
+        accordingly.
    
    The remove operation highlights the two limitations:
    (i)  duplicates are not allowed in the data structure
-   (ii) ordered operations are not suppored (this is implied anyway since it is not listed in the API.
+   (ii) ordered operations are not suppored (this is implied anyway since it is 
+        not listed in the API.
 
-   contains – Since every insert adds the value of the item as a key in the hash table, we are able to take advantage of the hash table's O(1) amortised look-up operation to support this in O(1) (amortised).
+   contains – Since every insert adds the value of the item as a key in the hash
+   table, we are able to take advantage of the hash table's O(1) amortised
+   look-up operation to support this in O(1) (amortised).
 
    random – generate a random number to select the item from the array at O(1).
 */
@@ -32,10 +42,13 @@ public class SpecialResizingArray<Item> implements Iterable<Item>
     private static final Random random = new Random();
 
     private Item[] values; // stores the given value
-    private Hashtable<Item, Integer> positions; // stores the position of the given value in the values array
+    private Hashtable<Item, Integer> positions; // stores the position of the 
+                                                // given value in the values array
 
     private int capacity;  // capacity of the values array
-    private int nextItemIndex; // index at which the next value will be added; can also be used to query the number of elements, aka (size), since the array is zero-indexed.
+    private int nextItemIndex; // index at which the next value will be added;
+                               // can also be used to query the number of elements, 
+                               // aka (size), since the array is zero-indexed.
 
     public SpecialResizingArray(int capacity)
     {
@@ -47,10 +60,7 @@ public class SpecialResizingArray<Item> implements Iterable<Item>
     }
 
     // the default constructor sets the capacity to DEFAULT_CAPACITY
-    public SpecialResizingArray()
-    {
-        this(DEFAULT_CAPACITY);
-    }
+    public SpecialResizingArray() { this(DEFAULT_CAPACITY); }
 
     // throws new IllegalArgumentException if item is null.
     // stores the item in the values array.
@@ -144,25 +154,16 @@ public class SpecialResizingArray<Item> implements Iterable<Item>
 
     // returns an iterator that iterates over the items in the values array in
     // arbitrary order
-    public Iterator<Item> iterator()
-    {
-        return new SpecialResizingArrayIterator();
-    }
+    public Iterator<Item> iterator() { return new SpecialResizingArrayIterator(); }
 
     // the iterator doesn't implement remove() since it's optional
     private class SpecialResizingArrayIterator implements Iterator<Item>
     {
         private int index;
 
-        public SpecialResizingArrayIterator()
-        {
-            index = 0;    
-        }
+        public SpecialResizingArrayIterator() { index = 0; }
 
-        public boolean hasNext()
-        {
-            return index < size();
-        }
+        public boolean hasNext() { return index < size(); }
 
         public void remove() { throw new UnsupportedOperationException(); }
 
@@ -185,4 +186,3 @@ public class SpecialResizingArray<Item> implements Iterable<Item>
         for (String s: array) System.out.println(array.random());
     }
 }
-
