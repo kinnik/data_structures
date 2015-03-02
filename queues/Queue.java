9d1a02368b240cf2be2a4051628835201613aca5
kinnik <northislandking@gmail.com>
2015-03-02 11:45:43 +0000

    Fixed the bug in dequeue(); Made it generic; added an iterator

--- queues/Queue.java
+++ queues/Queue.java
@@ -9,23 +9,20 @@ package data_structures.queues;
 
 import java.util.*;
 
-public class Queue<Item>
-{
-    private Node first, last;
+public class Queue<Item> implements Iterable<Item> {
+    private Node<Item> first, last;
     private int count;
 
-    public Queue()
-    {
+    public Queue() {
         first = null;
         last  = null;
         count = 0;
     }
 
-    public void enqueue(Item item)
-    {
-        Node oldlast = last;
+    public void enqueue(Item item) {
+        Node<Item> oldlast = last;
 
-        last = new Node();
+        last = new Node<Item>();
         last.item = item;
         last.next = null;
 
@@ -35,28 +32,79 @@ public class Queue<Item>
         ++count;
     }
 
-    public Item dequeue()
-    {
+    public Item dequeue() {
         if (isEmpty()) throw new NoSuchElementException("Queue underflow");
 
         Item item = first.item;
 
         first = first.next;
 
-        if (isEmpty()) last = null;
-
         --count;
+
+        if (isEmpty()) last = null;
 
         return item;
     }
 
-    public boolean isEmpty() { return first == null; }
+    public boolean isEmpty() {
+        return first == null;
+    }
 
-    public int size() { return count; }
+    public int size() {
+        return count;
+    }
 
-    private class Node
-    {
+    public Iterator<Item> iterator() {
+        return new QueueIterator<Item>(first);
+    }
+
+    private class QueueIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public QueueIterator(Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item item = current.item;
+            current   = current.next;
+            return item;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private class Node<Item> {
         Item item;
-        Node next;
+        Node<Item> next;
+    }
+
+    public static void main(String[] args) {
+        Queue<String> q = new Queue<String>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String item = scanner.next();
+            if (!item.equals("-")) {
+                q.enqueue(item);
+            } else if (!q.isEmpty()) {
+                System.out.println(q.dequeue() + " ");
+            }
+            System.out.println("(" + q.size() + " left on queue)");
+        }
+
+        for (String s: q) {
+            System.out.print(s + " ");
+        }
+        System.out.println("");
     }
 }
